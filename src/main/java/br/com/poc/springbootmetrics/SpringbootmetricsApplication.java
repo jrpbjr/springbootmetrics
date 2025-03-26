dc451aafd8f0c859fff6753c85d8e93f6392ff99
jrpbjr <jrpbjr@gmail.com>
2025-03-25 22:42:49 -0300

    first commit

---
+++ src/main/java/br/com/poc/springbootmetrics/SpringbootmetricsApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.springbootmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmetricsApplication.class, args);
+    }
+
+}
